fee8fb490249f4293f74ac6490e5680c84247caf
sahidularif <sahidularif1@gmail.com>
2021-09-07 08:02:32 +0600

    server running

--- server.js
+++ server.js
@@ -1,1 +1,98 @@
-console.log('hi');
+/* 
+**
+** Project Name: 
+** Author: 
+** Date:  
+**
+*/
+// Defendency
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload');
+require('dotenv').config();
+
+// app config
+const port = 9000;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('service'));
+app.use(fileUpload());
+
+// Mongodb connection
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.or4h7.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const articleCollection = client.db('truthspeech').collection('articles');
+    const adminCollection = client.db('truthspeech').collection('admins');
+
+    // URSER ROUTES
+    //:::::::::::::::::::::::::: ADD ARTICLE ::::::::::::::::::::::
+
+    app.post('/post-article', (req, res) => {
+        const file = req.files.file;
+        const title = req.body.title;
+        const description = req.body.description;
+        const author = req.body.authorreq.body.author;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64'),
+        };
+
+        articleCollection
+            .insertOne({ author, title, description, image })
+            .then((result) => {
+                res.send(result.insertedCount > 0);
+            });
+    });
+
+    //:::::::::::::::::::::::: ADD ADMIN::::::::::::::::::::::::::
+    app.post('/addAdmin', (req, res) => {
+        const newAdmin = req.body;
+        adminCollection.insertOne(newAdmin).then((result) => {
+            // console.log(result)
+            res.send(result.insertedCount > 0);
+        });
+    });
+
+    //:::::::::::::::::::: VERIFY ADMIN LOGIN ::::::::::::::::::::
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.find({ email: email })
+            .toArray((err, admins) => {
+                console.log(admins);
+                res.send(admins.length > 0);
+            })
+    })
+
+    //::::::::::::::::::::::: GET ALL ARTICLE ::::::::::::::::::::::
+    app.get('/reviews', (req, res) => {
+        feedbackCollection.find({}).toArray((err, documents) => {
+            res.send(documents);
+        });
+    });
+
+});
+
+// Root:
+app.get('/', (req, res) => {
+    res.send(`
+    <html>
+    <head><title>The Truth Speech</title><style>h1{color:red;}</style></head>
+    <body><marquee><h1>THANKS FOR TRUTHSPEECH</h1></marquee></body>
+    </html>
+    `);
+});
+
+// Listener port
+app.listen(process.env.PORT || port);
